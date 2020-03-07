2699ad92233010d26c4f8d369b89205bfca8c129
Jiju Thomas Mathew <jijutm@gmail.com>
2020-03-07 23:00:17 +0530

    AWS/Cloud Watch UAPI xml logger

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # code-collection
 Just a collection of code snippets
+
+* [AWS/Cloud Watch UAPI xml logger](./aws/cloud-watch-uapi-xml-logger.js) 
+
+
+## AWS/Cloud Watch UAPI xml logger
+
+### Introduction
+
+[TravelPort UAPI](https://support.travelport.com/webhelp/uapi/uAPI.htm) Travelport Universal API offers an array of travel content for air, hotel, car, and rail, including ancillaries (optional services). It also provides functionality to build complete traveler, agency, branch, and account profiles. 
+
+[TravelPort-Ukrane uapi-json](https://github.com/Travelport-Ukraine/uapi-json) Best Travelport Universal API wrapper ever, needed some extra features like xml logging ([Issue:391](https://github.com/Travelport-Ukraine/uapi-json/issues/391#issuecomment-595560699)). This provides a solution to the same as suggested.
+
+### Guidelines
+
+Create the Cloud Watch Log Group and provide through environment variable (cwLogGroup) or constant in code line 12 value for logGroupName. AWS_LAMBDA_LOG_STREAM_NAME is a [Runtime Reserved Environment Variable](https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html), require this library and set the entry point as logFunction property of options property of settings object as specified [here](https://github.com/Travelport-Ukraine/uapi-json#options). 
---
+++ aws/cloud-watch-uapi-xml-logger.js
@@ -0,0 +1,73 @@
+/* jshint esversion: 8 */
+const AWS = require("aws-sdk");
+const cloudwatchlogs = new AWS.CloudWatchLogs({ apiVersion: '2014-03-28' });
+
+const typeCheck = function(v, t){
+  return Object.prototype.toString.call(v) === '[object ' + t + ']';
+};
+
+async function tryCreateStream() {
+
+  params = {
+    logGroupName: process.env.cwLogGroup, /* required */
+    logStreamName: process.env.AWS_LAMBDA_LOG_STREAM_NAME /* required */
+  };
+
+  doAction = new Promise((s, f) => {
+    cloudwatchlogs.createLogStream(params, function (err, data) {
+      if (err) {
+        if(err.code === 'ResourceAlreadyExistsException'){
+          s();
+        }else{
+          f(err);
+        }
+      }
+      else {
+        console.log(data);           // successful response
+        s();
+      }
+    });
+  });
+
+  await doAction;
+
+  return params;
+}
+
+async function tryWriteLog(logStream, records) {
+  var count = (arguments.length > 2) ? arguments[2] : 0;
+  if(count >= 4){
+    throw new Error("Attempted thrice.. failing");
+  }
+  var params = {
+    ...logStream,
+    logEvents: records
+  };
+  let doAction = new Promise((s, f) => {
+    cloudwatchlogs.putLogEvents(params, async function (err, data) {
+      if (err) {
+        console.log(err, err.stack); // an error occurred
+        if(err.code === 'InvalidSequenceTokenException' && typeCheck(err.expectedSequenceToken, 'String')){
+          logStream.sequenceToken = err.expectedSequenceToken;
+          return await tryWriteLog(logStream, records, count++);
+        }
+      }
+      else {
+        console.log(data);           // successful response
+        s();
+      }
+    });
+  });
+  await doAction;
+}
+
+module.exports = async function (message, params) {
+  if(typeCheck(process.env.AWS_SAM_LOCAL, 'Undefined') || (process.env.AWS_SAM_LOCAL !== 'true')){
+    if (message === 'Request XML: ' || message === 'Response SOAP: ') {
+      const logStream = await tryCreateStream();
+      await tryWriteLog(logStream, [params]);
+      return;
+    }      
+  }
+  console.log(message, params);
+};
