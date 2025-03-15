2c0050152e79e651c387261372279ceb26c995a6
Jiju Thomas Mathew <jijutm@gmail.com>
2025-03-15 21:43:23 +0530

    Further log file messages while running

--- aws/ec2-inactivity-shutdown/runner.sh
+++ aws/ec2-inactivity-shutdown/runner.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+LOG_FILE="/var/log/autostop.log"
 if [ -f /root/longrun ] ; then
+ echo "[$(date)] longrun exists skipping shutdown" >> $LOG_FILE 
  exit
 fi
 
-
-/root/autostop.sh >> /var/log/autostop.log 2>&1 &
+/root/autostop.sh >> $LOG_FILE 2>&1 &
 
 sleep 30
 
-/root/autostop.sh >> /var/log/autostop.log 2>&1 &
+/root/autostop.sh >> $LOG_FILE 2>&1 &
