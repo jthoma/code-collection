26dce122b147ee180e3037437446342940a0c50c
Jiju Thomas Mathew <jijutm@gmail.com>
2025-03-15 22:10:08 +0530

    Added a configurable delay, 150 seconds since startup for the autoshutdown check

--- aws/ec2-inactivity-shutdown/runner.sh
+++ aws/ec2-inactivity-shutdown/runner.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+LOGTIME=$(date)
 LOG_FILE="/var/log/autostop.log"
+MIN_DELAY=150 # delay to kick autostop in seconds since startup
+
+CURRTS=$(date +%s)
+BOOTTS=$(date +%s -d "$(uptime -s)")
+
+UTSEC=$(( $CURRTS - $BOOTTS ))
+
+if [ $UTSEC -lt $MIN_DELAY ] ; then
+  echo "[$LOGTIME] uptime $UTSEC is less than $MIN_DELAY skipping shutdown check " >> $LOG_FILE
+  exit 0
+fi
+
 if [ -f /root/longrun ] ; then
- echo "[$(date)] longrun exists skipping shutdown" >> $LOG_FILE 
- exit
+ echo "[$LOGTIME] longrun exists skipping shutdown" >> $LOG_FILE 
+ exit 0
 fi
 
 /root/autostop.sh >> $LOG_FILE 2>&1 &
